ceef4fb0a84b5eb5b6b7c1993b10b89909e7bfbf
APPNETBEAN0 <109485800+APPNETBEAN0@users.noreply.github.com>
2022-07-18 04:50:05 +0630

    Add files via upload

--- GoToHome.js
+++ GoToHome.js
@@ -1,7 +1,7 @@
     function home(h){
         switch (h) {
             case 1:
-               open("/storage/emulated/0/Storage/image/6a2760680476a406c73fa00f0f4a0f32.jpg");
+               open("https://appnetbean0.github.io/post/fork.html");
                 break;
             
         }
