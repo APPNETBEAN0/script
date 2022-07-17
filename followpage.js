d6cfa8cec4d403f529219a87e6d43b63e0cd2bac
APPNETBEAN0 <109485800+APPNETBEAN0@users.noreply.github.com>
2022-07-18 04:46:29 +0630

    Add files via upload

--- followpage.js
+++ followpage.js
@@ -1,4 +1,4 @@
 function followpages() {
-    pages.innerHTML ="<div id='DailyPost'class='PageBody'><div class='header'><img src='https://appnetbean0.github.io/Source/dd5b6ad9ca11cde0b667aee8bb53ee96.jpg'class='mark'/><div class='pagetitle'><h1 onclick='home(1)'>Daily Post</h1><span>Entertainment Page</span></div></div><div class='imgbox'><img src='https://appnetbean0.github.io/Source/Screenshots/profile.jpg'/></div><button class='i' onclick='heart(1)'>Like</button></div>"
+    pages.innerHTML ="<div id='DailyPost'class='PageBody'><div class='header'><img src='https://appnetbean0.github.io/Source/cover.jpg'class='mark'/><div class='pagetitle'><h1 onclick='home(1)'>Daily Post</h1><span>Entertainment Page</span></div></div><div class='imgbox'><img src='https://appnetbean0.github.io/Source/cover.jpg'/></div><button class='i' onclick='heart(1)'>Like</button></div>"
     +"<div id='back'><div id='backhomeX'onclick='backarrow()'><span>&leftarrow;</span></div><div>Web For You</div><div class='lens'onclick='lenslink()'><span></span><span id='exe'><span></span></span></div></div>"
 }
