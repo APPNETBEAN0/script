6147e72b64d789b495ed826777f726379252d998
APPNETBEAN0 <109485800+APPNETBEAN0@users.noreply.github.com>
2022-07-18 06:00:24 +0630

    Add files via upload

--- frontpost.js
+++ frontpost.js
@@ -1,6 +1,6 @@
  function mainpost(){
      main.innerHTML ="<div id='postid1'class='postctn'><div class='net'><img src='https://appnetbean0.github.io/Source/cover.jpg'class='mark'/><div class='pagetitle'><h1 onclick='home(1)'>Daily Post</h1><h1 onclick='dislike(01)'>dislike</h1><span>Entertainment Website</span></div></div><div class='cover'><img onclick='visit(1)'src='https://appnetbean0.github.io/Source/received_405222651625750.jpeg'/></div></div>"
      +
-     "<div id='postid1'class='postctn'><div class='net'><img src='https://appnetbean0.github.io/Source/cover.jpg'class='mark'/><div class='pagetitle'><h1 onclick='home(1)'>Daily Post</h1><h1 onclick='dislike(01)'>dislike</h1><span>Entertainment Website</span></div></div><div class='cover'><img onclick='visit(2)'src='https://appnetbean0.github.io/Source/today.jpg.jpeg'/></div></div>"
+     "<div id='postid1'class='postctn'><div class='net'><img src='https://appnetbean0.github.io/Source/cover.jpg'class='mark'/><div class='pagetitle'><h1 onclick='home(1)'>Daily Post</h1><h1 onclick='dislike(01)'>dislike</h1><span>Entertainment Website</span></div></div><div class='cover'><img onclick='visit(2)'src='https://appnetbean0.github.io/Source/today.jpg'/></div></div>"
      
  }
